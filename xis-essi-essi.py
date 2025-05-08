c34faf8b507c1ccb4eadbf4e82034c68dc937da8
waslacy <wallace201269wzs@gmail.com>
2025-05-08 10:46:21 -0300

    translated comments

--- xis-essi-essi.py
+++ xis-essi-essi.py
@@ -2,60 +2,52 @@ import yaml
 import argparse
 import sys
 
-# ===== Carrega yaml =====
+# ===== Loads yaml =====
 def load_config(path="config.yaml"):
   try:
     with open(path, "r") as file:
       return yaml.safe_load(file)
   except Exception as e:
-    print(f"[!] Erro ao carregar config.yaml: {e}")
+    print(f"[!] Fail to load config.yaml: {e}")
     sys.exit(1)
   
   
 
-# ===== Carrega lista de payloads =====
+# ===== Load payloads list =====
 def load_payloads(payload_file):
   try:
     with open(payload_file, "r") as file:
       return [line.strip() for line in file if line.strip()]
     
   except FileNotFoundError:
-    print(f"[!] Não foi possível encontrar o arquivo: {payload_file}")
+    print(f"[!] File Not Found: {payload_file}")
     sys.exit(1)
   
   
-# ===== Tela de Boas-Vindas =====
+# ===== Banner screen =====
 def show_banner(mode):
-    if mode == "full":
-      print("""
+    print("""
 ╔════════════════════════════════════════════════════╗
 ║             Xis Essi Essi - XSS Scanner            ║
 ║        Scanner simples e extensível para XSS       ║
 ║     Desenvolvido para fins educacionais e testes   ║
 ╚════════════════════════════════════════════════════╝
+""")
 
-Uso:
+    if mode == "full": 
+      print("""Uso:
     python3 xis-essi-essi.py -u "https://alvo.com"
 
 Opções:
     -u / --url        Site alvo para teste (obrigatório)
     -p / --payloads   Caminho para o arquivo de payloads (opcional, arquivo padrão no yaml)
 """)
-      
-    else: 
-      print("""
-╔════════════════════════════════════════════════════╗
-║             Xis Essi Essi - XSS Scanner            ║
-║        Scanner simples e extensível para XSS       ║
-║     Desenvolvido para fins educacionais e testes   ║
-╚════════════════════════════════════════════════════╝
-""")
   
   
-# ===== Modifica ArgumentParser para adicionar tela no -h =====
+# ===== Modify ArgumentParser to add the banner on -h =====
 class CustomArgumentParser(argparse.ArgumentParser):
     def print_help(self, file=None):
-        show_banner("basic")  # Exibe o banner quando -h ou --help é chamado
+        show_banner("basic")
         super().print_help(file)
   
   
@@ -69,8 +61,8 @@ if __name__ == "__main__":
   
   # CLI args
   parser = CustomArgumentParser(description="XSS Scanner (xis-essi-essi!)")
-  parser.add_argument("-u", "--url", required=True, help="Domínio alvo")
-  parser.add_argument("-p", "--payloads", default=config.get("payloads_file", "payloads.txt"), help="Arquivo com os payloads que serão testados")
+  parser.add_argument("-u", "--url", required=True, help="Target Site")
+  parser.add_argument("-p", "--payloads", default=config.get("payloads_file", "payloads.txt"), help="Payloads list")
   args = parser.parse_args()
   
   # YAML params
@@ -91,11 +83,11 @@ if __name__ == "__main__":
   print(f"use_browser:  {use_browser}")
   print(f"payload file: {args.payloads}")
   
-  print("\n===== PAYLOADS CARREGADOS ======")
+  print("\n======= PAYLOADS LOADED ========")
   
   if payloads:
     for i, p in enumerate(payloads, 1):
       print(f"{i:02d}: {p}")
       
   else:
-    print("Nenhum payload encontrado!")
+    print("[!] No payloads found!")
